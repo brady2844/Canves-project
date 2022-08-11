785e5a68d111d25e9f3ce9c90e39d711f11c4c29
brady2844 <107158782+brady2844@users.noreply.github.com>
2022-08-10 21:32:51 -0500

    first commit

---
+++ scripts/button.js
@@ -0,0 +1,5 @@
+const canvasBtn = document.getElementById('Canvas')
+const microsoftWordBtn = document.getElementById('microsoft-word')
+const extraBtn = document.getElementById('extra')
+
+window.open(gsboe.instructure.com, 'Canvas')
